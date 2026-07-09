61b613176b8f5b826d78d0c185ab7d4308c5a6d0
yanggu0413 <yanggu20150413@gmail.com>
2026-07-09 14:56:11 +0800

    remove fabric-mod from tracking and ignore it entirely

--- fabric-mod/src/main/java/com/craftcore/config/ModConfig.java
+++
@@ -1,7 +0,0 @@
-package com.craftcore.config;
-
-public class ModConfig {
-    public String websocketUrl = "ws://localhost:8080";
-    public String serverSecret = "shared_secret_token";
-    public String discordInvite = "https://discord.gg/your-invite";
-}
