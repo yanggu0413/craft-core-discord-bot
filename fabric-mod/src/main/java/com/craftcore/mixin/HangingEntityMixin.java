caf96ecfa9ad19b8f29956be673c432b373e7daf
yanggu0413 <yanggu20150413@gmail.com>
2026-08-04 06:43:18 +0800

    fix: target Entity.class in HangingEntityMixin to fix hurtServer mixin injection on HangingEntity

--- fabric-mod/src/main/java/com/craftcore/mixin/HangingEntityMixin.java
+++ fabric-mod/src/main/java/com/craftcore/mixin/HangingEntityMixin.java
@@ -5,27 +5,31 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.decoration.ArmorStand;
 import net.minecraft.world.entity.decoration.HangingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(HangingEntity.class)
+@Mixin(Entity.class)
 public class HangingEntityMixin {
     @Inject(method = "hurtServer", at = @At("HEAD"), cancellable = true)
     private void onHurtServer(ServerLevel level, DamageSource damageSource, float amount, CallbackInfoReturnable<Boolean> cir) {
-        HangingEntity entity = (HangingEntity) (Object) this;
-        BlockPos pos = entity.blockPosition();
-        ClaimManager.Claim claim = ClaimManager.getClaimAt(pos, level);
-        if (claim != null) {
-            if (damageSource.getEntity() instanceof ServerPlayer player) {
-                if (!ClaimManager.checkPermission(player, pos, level, "break") && !ClaimManager.checkPermission(player, pos, level, "interact")) {
-                    cir.setReturnValue(false);
-                }
-            } else {
-                if (claim.explosion_protection) {
-                    cir.setReturnValue(false);
+        Entity entity = (Entity) (Object) this;
+        if (entity instanceof HangingEntity || entity instanceof ArmorStand) {
+            BlockPos pos = entity.blockPosition();
+            ClaimManager.Claim claim = ClaimManager.getClaimAt(pos, level);
+            if (claim != null) {
+                if (damageSource.getEntity() instanceof ServerPlayer player) {
+                    if (!ClaimManager.checkPermission(player, pos, level, "break") && !ClaimManager.checkPermission(player, pos, level, "interact")) {
+                        cir.setReturnValue(false);
+                    }
+                } else {
+                    if (claim.explosion_protection) {
+                        cir.setReturnValue(false);
+                    }
                 }
             }
         }
