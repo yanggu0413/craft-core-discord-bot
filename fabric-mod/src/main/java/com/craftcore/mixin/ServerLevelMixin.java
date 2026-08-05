d2492f26d0b609ca80c24b7265d6808757edfdad
yanggu0413 <yanggu20150413@gmail.com>
2026-08-05 18:07:23 +0800

    feat: intercept Wither boss and Mob entity spawning in craftcore:fishing dimension

---
+++ fabric-mod/src/main/java/com/craftcore/mixin/ServerLevelMixin.java
@@ -0,0 +1,26 @@
+package com.craftcore.mixin;
+
+import com.craftcore.fish.FishingContestManager;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.boss.wither.WitherBoss;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ServerLevel.class)
+public class ServerLevelMixin {
+
+    @Inject(method = "addFreshEntity", at = @At("HEAD"), cancellable = true)
+    private void onAddFreshEntity(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        ServerLevel level = (ServerLevel) (Object) this;
+        if (level.dimension().equals(FishingContestManager.FISHING_DIMENSION_KEY)) {
+            if (entity instanceof Mob || entity instanceof WitherBoss) {
+                cir.setReturnValue(false);
+            }
+        }
+    }
+}
