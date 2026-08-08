21b02912fa6a9691400f6a03ed5f8f6b31389e6d
yanggu0413 <yanggu20150413@gmail.com>
2026-08-08 08:40:57 +0800

    fix(fabric-mod-public): align fabric.mod.json depends/suggests, clean redundant deps, drop monolithic root src

--- fabric-mod-public/src/main/java/com/craftcore/config/ModConfig.java
+++
@@ -1,6 +0,0 @@
-package com.craftcore.config;
-
-public class ModConfig {
-    public String serverSecret = "shared_secret_token";
-    public String discordInvite = "https://discord.gg/your-invite";
-}
